fbe3b8943c3bd37ae7288427bb7e7c043b69ad09
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-09-30 22:19:32 +0700

    done

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,5 +11,17 @@ Page({
     let userId = app.globalData.userId
     // set userId in data state
     this.setData({ userId : userId })
+  },
+
+  bindKeyInput: function(e) {
+    // Get New User Id from input
+    let newUserId = e.detail.value
+    // Update in data state
+    this.setData({ userId : newUserId })
+    // Update in GlobalData
+    app.globalData.userId = newUserId
+    // Check new value in GlobalData
+    console.log(app.globalData) 
+    
   }
 })
